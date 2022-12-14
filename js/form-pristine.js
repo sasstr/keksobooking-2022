900f2e387526ea1df591b14719da3812886eb5ed
Alexander Strakhovenko <sasstr@gmail.com>
2022-12-14 18:47:32 +0300

    fix: Исправляет разметку index.html

---
+++ js/form-pristine.js
@@ -0,0 +1,11 @@
+
+
+const pristine = new Pristine(form, {
+  classTo: 'ad-form__element',
+  errorClass: 'form__item--invalid',
+  successClass: 'form__item--valid',
+  errorTextParent: 'ad-form__element',
+  errorTextTag: 'span',
+  errorTextClass: 'ad-form__error-text',
+});
+
