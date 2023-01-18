fb43b78d6b286c75f2669d5c1b6cbe9ba4cfd48a
Alexander Strakhovenko <sasstr@gmail.com>
2023-01-18 22:22:48 +0300

    fix: Добавляет потановку задачи на дз8-1 и востанавливает загрузку всех пинов

--- js/filter.js
+++ js/filter.js
@@ -1,6 +1,6 @@
 
-const housingType = document.querySelector('#housing-type');
-const housingPrice = document.querySelector('#housing-price');
-const housingRooms = document.querySelector('#housing-rooms');
-const housingGuests = document.querySelector('#housing-guests');
-const housingFeatures = document.querySelector('#housing-features');
+// const housingType = document.querySelector('#housing-type');
+// const housingPrice = document.querySelector('#housing-price');
+// const housingRooms = document.querySelector('#housing-rooms');
+// const housingGuests = document.querySelector('#housing-guests');
+// const housingFeatures = document.querySelector('#housing-features');
